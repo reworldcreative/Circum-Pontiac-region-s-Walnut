07224e688e847e9b4a97d4d45f3305a807e596b3
reworldcreative <reworldcreative@gmail.com>
2023-05-16 10:49:13 +0300

    create one product

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 import * as functions from "./modules/functions.js";
 
 functions.isWebp();
+
+$(document).ready(function () {
+  if ($(".burger-btn")) {
+    $(".burger-btn").click(function (event) {
+      $(".burger-btn,.burger__menu").toggleClass("show");
+    });
+  }
+});
+
+if (document.getElementById("play-btn")) {
+  document.getElementById("play-btn").onclick = function (event) {
+    fadeImage(event);
+  };
+
+  function fadeImage(event) {
+    document.getElementById("poster-image").style.zIndex = "0";
+    document.getElementById("video").style.zIndex = "30";
+  }
+}
