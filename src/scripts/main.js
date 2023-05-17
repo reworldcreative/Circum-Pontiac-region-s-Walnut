ec42c3cb30d2a95d05f051874abff41147222aad
reworldcreative <reworldcreative@gmail.com>
2023-05-17 12:48:20 +0300

    create products list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,3 +20,28 @@ if (document.getElementById("play-btn")) {
     document.getElementById("video").style.zIndex = "30";
   }
 }
+
+if ($(".search")) {
+  $(".search").click(function (target) {
+    // target.currentTarget.parentElement.id
+    $(
+      "#" +
+        target.currentTarget.parentElement.getElementsByTagName("div")[0].id +
+        " " +
+        ".swiper-slide-active .product__image"
+    ).toggleClass("scale");
+  });
+
+  $(".product__image").mouseleave(function () {
+    for (let index = 0; index < $(".product__image").length; index++) {
+      $(".product__image")[index].classList.remove("scale");
+    }
+  });
+}
+
+const productSwiper = new Swiper(".product__swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
