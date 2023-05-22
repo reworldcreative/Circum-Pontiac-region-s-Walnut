f0e8080bf1206c279ed75460ae63305579d27861
reworldcreative <reworldcreative@gmail.com>
2023-05-22 21:14:24 +0300

    add wholesale and payment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -49,6 +49,52 @@ if ($(".search")) {
   });
 }
 
+if ($(".tabs-navigation__item")) {
+  $(".tabs-navigation__item").click(function (el) {
+    console.log(el.currentTarget.getAttribute("name"));
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabs__item");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tabs-navigation__item");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(
+      el.currentTarget.getAttribute("name")
+    ).style.display = "flex";
+
+    el.currentTarget.className += " active";
+  });
+}
+
+if ($(".pay-tabs-navigation__item")) {
+  $(".pay-tabs-navigation__item").click(function (el) {
+    console.log(el.currentTarget.getAttribute("name"));
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("pay-tabs__item");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("pay-tabs-navigation__item");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(
+      el.currentTarget.getAttribute("name")
+    ).style.display = "flex";
+
+    el.currentTarget.className += " active";
+  });
+}
+
 const productSwiper = new Swiper(".product__swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
