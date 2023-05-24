497ae6e645beb6e69b0a48d43d414d3a1348ed19
reworldcreative <reworldcreative@gmail.com>
2023-05-24 20:28:43 +0300

    add shop and product item

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -95,6 +95,29 @@ if ($(".pay-tabs-navigation__item")) {
   });
 }
 
+if ($(".mainProduct-Info__item")) {
+  $(".mainProduct-Info__item").click(function (el) {
+    console.log(el.currentTarget.getAttribute("name"));
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tab__item");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("mainProduct-Info__item");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(
+      el.currentTarget.getAttribute("name")
+    ).style.display = "flex";
+
+    el.currentTarget.className += " active";
+  });
+}
+
 const productSwiper = new Swiper(".product__swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
