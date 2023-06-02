743e56d9fc14686ce70ae0c7dd014e3b359be733
reworldcreative <reworldcreative@gmail.com>
2023-06-02 17:45:01 +0300

    add gallery 404 abot cabinet login payment

---
+++ src/scripts/counter.js
@@ -0,0 +1,23 @@
+if ($(".number-counter")) {
+}
+
+function counter() {
+  $(".number-counter .minus").click(function () {
+    var $input = $(this).parent().find("input");
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+  });
+  $(".number-counter .plus").click(function () {
+    var $input = $(this).parent().find("input");
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+  });
+}
+
+$(document).ready(function () {
+  counter();
+});
